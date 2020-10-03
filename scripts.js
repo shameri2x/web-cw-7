bf3c34eb681e19ca058ac61d5ff56f91316cd4f8
AhmedAlshammeri-46 <66562839+shameri2x@users.noreply.github.com>
2020-10-03 20:48:25 +0300

    done

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ const counter = document.querySelector(".counterButton");
 
 // 👇🏻 مكان النقاط count قم بتعيين القيمة صفر إلى المتغير
 // ...
+let count = 0 ;
 
 counter.addEventListener("click", incrementCounter);
 
@@ -13,11 +14,20 @@ counter.addEventListener("click", incrementCounter);
 function incrementCounter() {
   // 👇🏻 بواحد مكان النقاط count قم بزيادة قيمة
   // ...
+  count++
   counter.innerHTML = count;
   // "تسوي 1 اطبع "جيد count إن كانت قيمة العداد أو
-  if (condition) {
+  if (count == 1) {
     document.getElementById("status").innerHTML = "جيد";
   }
   // "أكبر من 1 وأقل من 6 اطبع "جيد جداً count إن كانت قيمة العداد أو
   // "أكبر من أو تساوي 6 اطبع "رائع count إن كانت قيمة العداد أو
+
+  if (count > 1 && count < 6) {
+
+    document.getElementById("status").innerHTML = "جيد جدا";
+  }
+  else if (count >= 6) {
+    document.getElementById("status").innerHTML = "رائع";
+  }
 }
